32d2752527b3473c254fb8d0e4f8d848558b543c
Akshay P <akshay.pendbhaje@gmail.com>
2019-02-14 22:28:45 +0530

    initial commit

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="hello">
+    <ion-label>Label</ion-label>
+
+    <!-- Label Colors -->
+    <ion-label color="primary">Primary Label</ion-label>
+
+    <ion-input clearInput value="clear me"></ion-input>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "HelloWorld",
+  props: {
+    msg: String
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
