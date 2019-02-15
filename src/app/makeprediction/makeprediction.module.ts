7fa4e36b4c30eb28ce7647c5e6d33444acb58d0b
Anirudh Loya <anirudhloya93@gmail.com>
2019-02-15 23:04:09 +0530

    Added menu in all pages

--- src/app/makeprediction/makeprediction.module.ts
+++ src/app/makeprediction/makeprediction.module.ts
@@ -23,4 +23,22 @@ const routes: Routes = [
   ],
   declarations: [MakepredictionPage]
 })
-export class MakepredictionPageModule {}
+export class MakepredictionPageModule {
+  customAlertOptions: any = {
+    header: 'Pizza Toppings',
+    subHeader: 'Select your toppings',
+    message: '$1.00 per topping',
+    translucent: true
+  };
+
+  customPopoverOptions: any = {
+    header: 'Hair Color',
+    subHeader: 'Select your hair color',
+    message: 'Only select your dominant hair color'
+  };
+
+  customActionSheetOptions: any = {
+    header: 'Colors',
+    subHeader: 'Select your favorite color'
+  };
+}
