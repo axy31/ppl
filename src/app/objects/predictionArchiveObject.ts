cefbe3f66335c1e98b61221ce2120f909fb08be9
Akshay P <akshay.pendbhaje@somaiya.edu>
2019-03-15 23:06:50 +0530

    prediction archive ts and object added

--- src/app/objects/predictionArchiveObject.ts
+++ src/app/objects/predictionArchiveObject.ts
@@ -1,9 +1,13 @@
 export interface predictionArchiveObject {
   Name: string;
+  MatchWinner: string;
+  Score: string;
+  Wicket: string;
+  RunScorer: string;
+  WicketTaker: string;
+  ManOfMatch: string;
+  Fifties: string;
+  Boundaries: string;
+  ModifiedDate: string;
   Points: string;
-  Rank: string;
-  Team: string;
-  OrangeCap: string;
-  MVP: string;
-  PurpleCap: string;
 }
--- src/app/pages/prediction-archive/prediction-archive.component.ts
+++ src/app/pages/prediction-archive/prediction-archive.component.ts
@@ -1,15 +1,45 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from "@angular/core";
+import { predictionArchiveObject } from "src/app/objects/predictionArchiveObject";
+import { ApiCallService } from "src/app/core/api-call.service";
+import { MatTableDataSource, MatSort } from "@angular/material";
 
 @Component({
-  selector: 'app-prediction-archive',
-  templateUrl: './prediction-archive.component.html',
-  styleUrls: ['./prediction-archive.component.css']
+  selector: "app-prediction-archive",
+  templateUrl: "./prediction-archive.component.html",
+  styleUrls: ["./prediction-archive.component.css"]
 })
 export class PredictionArchiveComponent implements OnInit {
+  displayedColumns: string[] = [
+    "Name",
+    "MatchWinner",
+    "Score",
+    "Wicket",
+    "RunScorer",
+    "WicketTaker",
+    "ManOfMatch",
+    "Fifties",
+    "Boundaries",
+    "ModifiedDate",
+    "Points"
+  ];
+  dataSource = new MatTableDataSource<predictionArchiveObject>();
+  @ViewChild(MatSort) sort: MatSort;
 
-  constructor() { }
+  constructor(private api: ApiCallService) {}
 
-  ngOnInit() {
+  predictionArchieve() {
+    this.api.getPredictionArchieve().subscribe(
+      res => {
+        this.dataSource.data = res;
+        this.dataSource.sort = this.sort;
+      },
+      err => {
+        console.log(err);
+      }
+    );
   }
 
+  ngOnInit() {
+    this.predictionArchieve();
+  }
 }
