68e6726dd2f8e438b7d74e68a558ff37c5a52312
Akshay P <akshay.pendbhaje@somaiya.edu>
2019-03-20 23:45:31 +0530

    added forgot password ts

--- src/app/users/forgot-password/forgot-password.component.ts
+++ src/app/users/forgot-password/forgot-password.component.ts
@@ -1,15 +1,39 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { Router, ActivatedRoute } from "@angular/router";
+import { FormBuilder, FormGroup, Validators } from "@angular/forms";
+import { AbstractControl } from "@angular/forms";
+import { ApiCallService } from "src/app/core/api-call.service";
+import { ToasterService } from "src/app/core/toaster.service";
 
 @Component({
-  selector: 'app-forgot-password',
-  templateUrl: './forgot-password.component.html',
-  styleUrls: ['./forgot-password.component.css']
+  selector: "app-forgot-password",
+  templateUrl: "./forgot-password.component.html",
+  styleUrls: ["./forgot-password.component.css"]
 })
 export class ForgotPasswordComponent implements OnInit {
+  loginForm: FormGroup;
+  loading = false;
+  submitted = false;
+  returnUrl: string;
+  Username: AbstractControl;
+  mobileNo: AbstractControl;
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor(
+    private api: ApiCallService,
+    fb: FormBuilder,
+    private route: ActivatedRoute,
+    private toaster: ToasterService,
+    private router: Router
+  ) {
+    this.loginForm = fb.group({
+      Username: [""],
+      mobileNo: [""]
+    });
+    this.Username = this.loginForm.controls["Username"];
+    this.mobileNo = this.loginForm.controls["mobileNo"];
   }
 
+  ngOnInit() {
+    this.returnUrl = this.route.snapshot.queryParams["returnUrl"] || "/";
+  }
 }
